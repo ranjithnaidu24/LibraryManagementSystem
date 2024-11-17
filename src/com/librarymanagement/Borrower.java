f5efdb22e45d8c4f0d1a1b9a4503e1803e4e025e
ranjithnaidu24 <tndkranjithnaidu@gmail.com>
2024-11-17 11:33:14 +0530

    Book model done andborrower model partially done

--- src/com/librarymanagement/Borrower.java
+++ src/com/librarymanagement/Borrower.java
@@ -4,4 +4,35 @@ public class Borrower {
 	private String name;
 	private String mobile;
 	private String membershipId;
+
+	public Borrower(String name, String mobile, String membershipId) {
+		this.name = name;
+		this.mobile = mobile;
+		this.membershipId = membershipId;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMembershipId(String membershipId) {
+		this.membershipId = membershipId;
+	}
+
+	public String getMembershipId() {
+		return membershipId;
+	}
+
 }
